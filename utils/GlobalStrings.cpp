ad909459d8c388030f03b77411c9794a07c122dd
andsfonseca <andsf@live.com>
2022-04-22 21:10:50 -0300

    feat: functions to get international strings

---
+++ utils/GlobalStrings.cpp
@@ -0,0 +1,112 @@
+#include "StringExtensions.cpp"
+#include <filesystem>
+#include <json/json.h>
+
+#if defined(_WIN32) || defined(_WIN64)
+#else
+#include <limits.h>
+#include <unistd.h>
+#endif
+
+namespace GlobalStrings
+{
+    static Json::Value LocaleStrings;
+
+    static std::string GetLocale()
+    {
+#if defined(_WIN32) || defined(_WIN64)
+        LCID lcid = GetThreadLocale();
+        wchar_t localeName[LOCALE_NAME_MAX_LENGTH];
+        if (LCIDToLocaleName(lcid, localeName, LOCALE_NAME_MAX_LENGTH, 0) == 0)
+            return "en-us";
+
+        return Extensions::toLowerCase(Extensions::ConvertWideToUtf8(localeName));
+#else
+        std::string localeName1 = std::setlocale(LC_ALL, "");
+        std::string localeP = localeName1.substr(0, localeName1.find('.'));
+        return Extensions::toLowerCase(localeP);
+#endif
+    }
+
+    static std::string GetExecutableFolder()
+    {
+        std::string path;
+#if defined(_WIN32) || defined(_WIN64)
+        char result[MAX_PATH];
+        path = std::string(result, GetModuleFileName(NULL, result, MAX_PATH));
+        Extensions::findAndReplaceAll(path, "\\", "/");
+#else
+        char result[PATH_MAX];
+        ssize_t count = readlink("/proc/self/exe", result, PATH_MAX);
+        path = std::string(result, (count > 0) ? count : 0);
+#endif
+        size_t last_slash_idx = path.rfind('/');
+        if (std::string::npos != last_slash_idx)
+        {
+            path = path.substr(0, last_slash_idx);
+        }
+        return path;
+    }
+
+    static void LoadLanguageJson()
+    {
+        const std::string LANGUAGE_FOLDER = "/lang/";
+        const std::string LANGUAGE_DEFAULT = "en-us";
+
+        std::string folder = GetExecutableFolder();
+        std::string localeName = GetLocale();
+
+        std::ifstream file(folder + LANGUAGE_FOLDER + localeName + ".json");
+        if (!file)
+        {
+            file = std::ifstream(folder + LANGUAGE_FOLDER + LANGUAGE_DEFAULT + ".json");
+            if (!file)
+            {
+                std::cerr << "Corrupted Language Files" << std::endl;
+                exit(1);
+            }
+        }
+
+        std::stringstream buffer;
+        buffer << file.rdbuf();
+
+        Json::Reader reader;
+
+        if (!reader.parse(buffer.str(), LocaleStrings))
+        {
+            std::cerr << "Corrupted Language Files" << std::endl;
+            exit(1);
+        }
+    }
+
+    static std::string Get(std::string key)
+    {
+
+        if (key == "")
+            return "";
+
+        if (LocaleStrings.isNull())
+            LoadLanguageJson();
+
+        Json::Value child = LocaleStrings;
+        size_t aux = key.find(':');
+        std::string token = key.substr(0, aux);
+        key.erase(0, aux + 1);
+        std::string lastToken = "";
+
+        while (key != "" && token != key)
+        {
+            if (child[token].isNull())
+                return "";
+
+            child = child[token];
+
+            lastToken = token;
+            aux = key.find(':');
+            token = key.substr(0, aux);
+            key.erase(0, aux + 1);
+        }
+
+        return child[token].asString();
+    }
+}
--- utils/StringExtensions.cpp
+++ utils/StringExtensions.cpp
@@ -1,4 +1,11 @@
 #include <iostream>
+#include <codecvt>
+#include <string>
+#include <algorithm>
+
+#if defined(_WIN32) || defined(_WIN64)
+#include <Windows.h>
+#endif
 
 namespace Extensions
 {
@@ -14,5 +21,35 @@ namespace Extensions
             // Get the next occurrence from the current position
             pos = data.find(toSearch, pos + replaceStr.size());
         }
+    }
+
+#if defined(_WIN32) || defined(_WIN64)
+#pragma warning(push)
+#pragma warning(disable : 4267)
+    static std::string ConvertWideToUtf8(const std::wstring &wstr)
+    {
+        int count = WideCharToMultiByte(CP_UTF8, 0, wstr.c_str(), wstr.length(), NULL, 0, NULL, NULL);
+        std::string str(count, 0);
+        WideCharToMultiByte(CP_UTF8, 0, wstr.c_str(), -1, &str[0], count, NULL, NULL);
+        return str;
+    }
+
+    static std::wstring ConvertUtf8ToWide(const std::string &str)
+    {
+        size_t count = MultiByteToWideChar(CP_UTF8, 0, str.c_str(), str.length(), NULL, 0);
+        std::wstring wstr(count, 0);
+        MultiByteToWideChar(CP_UTF8, 0, str.c_str(), str.length(), &wstr[0], count);
+        return wstr;
+    }
+
+#pragma warning(pop)
+#endif
+
+    static std::string toLowerCase(std::string data)
+    {
+        std::transform(data.begin(), data.end(), data.begin(),
+                       [](unsigned char c)
+                       { return std::tolower(c); });
+        return data;
     }
 }
