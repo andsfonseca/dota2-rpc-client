e9e321b160d67b74d7fa752e9688a2d8c3addf29
andsfonseca <andsf@live.com>
2026-03-25 21:16:29 -0300

    docs: code area documentation

--- web/src/app/components/code-with-clipboard/code-with-clipboard.component.ts
+++ web/src/app/components/code-with-clipboard/code-with-clipboard.component.ts
@@ -10,10 +10,22 @@ import { HighlightModule } from 'ngx-highlightjs';
 })
 export class CodeWithClipboardComponent {
 
+  /**
+   * The code to be displayed and copied to the clipboard. This input is required and should be a string.
+   */
   @Input({ required: true }) text!: string;
-
+  /**
+   * A flag indicating whether the text has been copied to the clipboard.
+   */
   copied = false;
 
+  /**
+   * An method that copies the provided text to the clipboard using the Clipboard API. If the text 
+   * has already been copied, it does nothing. If the copy operation is successful, it sets the 
+   * `copied` flag to true for 1.5 seconds before resetting it to false. If an error occurs during 
+   * the copy operation, it logs the error to the console.
+   * @returns Promise that resolves when the copy operation is complete.
+   */
   async copy(): Promise<void> {
 
     if (this.copied)
